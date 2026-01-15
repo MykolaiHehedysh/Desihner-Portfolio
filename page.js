ce08c5129673c3a7d2e7b4d917299864e4810f93
MykolaiHehedysh <mikolagegedis04@gmail.com>
2026-01-15 19:36:30 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,24 @@
+
+const header = document.querySelector(".header")
+const burger = header.querySelector(".burger_menu")
+const burgerIcon = header.querySelector(".burger_menu_icon")
+
+
+console.log(header);
+console.log(burger);
+console.log(burgerIcon);
+
+
+burger.addEventListener("click", function() {
+  header.classList.toggle("header-mobile");
+
+  if (header.classList.contains("header-mobile")){
+    burgerIcon.src="images/Vector.svg"
+
+  }else{
+    burgerIcon.src=" images/Ham.png"
+
+  }
+
+  })
+  
